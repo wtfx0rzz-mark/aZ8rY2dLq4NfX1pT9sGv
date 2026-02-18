757dcf90fb9699f435bfc9068f67b5a205501513
wtfx0rzz-mark <wtfmark@pm.me>
2026-02-18 11:28:47 -0600

    Update URL for loading main.lua

--- stage.lua
+++ stage.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfx0rzz-mark/pub-hzkjsdfh983hcosedf-99/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfx0rzz-mark/aZ8rY2dLq4NfX1pT9sGv/main/main.lua"))()
