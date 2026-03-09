8736edc2de1207136e00087174145cdb008f0f79
wtfx0rzz-mark <wtfmark@pm.me>
2026-03-08 21:59:25 -0500

    Update diamonds.lua

--- diamonds.lua
+++ diamonds.lua
@@ -798,7 +798,7 @@ return function(C, R, UI)
 
     tab:Toggle({
         Title = "Auto AFK Action",
-        Default = (C.State.DiamondsAutoAfkAction ~= false),
+        Value = (C.State.DiamondsAutoAfkAction ~= false),
         Callback = function(on)
             C.State.DiamondsAutoAfkAction = on and true or false
         end
