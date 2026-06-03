3509c24438d88f8f436497e2c797cf90bc7c8494
wtfx0rzz-mark <wtfmark@pm.me>
2026-06-03 11:45:39 -0500

    Refactor air dropping parameters and wave calculation
    Updated MAX_AIR_DROPPING to use MAX_CONCURRENT-1 and adjusted waveY calculation for drop position.

--- tpbring.lua
+++ tpbring.lua
@@ -154,7 +154,7 @@ return function(C, R, UI)
     local AIR_DROP_TIMEOUT_S      = 2.25
     local AIR_RETRY_PUSH_DOWN_VY  = -80
 
-    local MAX_AIR_DROPPING        = 20
+    local MAX_AIR_DROPPING        = MAX_CONCURRENT-1
     local AIR_DROP_SPREAD_RADIUS  = 0.6
 
     local SCRAPPER_CHUTE_ABOVE    = 12
@@ -781,7 +781,7 @@ return function(C, R, UI)
             if m and m.Parent and rec then
                 local targetXZ = entry.targetXZ
                 local waveIndex = airDroppingCount
-		local waveY = math.sin(waveIndex * 10) * .3
+		local waveY = math.sin(waveIndex * 50) * .05
 		local dropPos = Vector3.new(
     		targetXZ and targetXZ.X or dropTarget.X,
     		dropTarget.Y + SCRAPPER_CHUTE_ABOVE + waveY,
